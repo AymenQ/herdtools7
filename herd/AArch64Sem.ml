631bd715b86d2a4aefdc79520182c99b9318e41e
Aymen Qader <aq1g17@soton.ac.uk>
2020-12-21 16:36:54 +0000

    [herd] Semantic support for LD1/ST1 (single-element structures)
    Add semantics support for the LD1 and ST1 instructions (single-element
    structures), including offset variants.
    
    Signed-off-by: Aymen Qader <aq1g17@soton.ac.uk>

--- herd/AArch64Sem.ml
+++ herd/AArch64Sem.ml
@@ -750,6 +750,16 @@ module Make
         end >>=
         fun v -> write_reg_neon_sz sz r1 v ii >>! B.Next
 
+      let load_elem sz r i addr ii =
+        let access_size = AArch64.simd_mem_access_size [r] in
+        do_read_mem access_size AArch64.N addr ii >>= fun v ->
+        write_reg_neon_elem sz r i v ii
+
+      let store_elem r i addr ii =
+        let access_size = AArch64.simd_mem_access_size [r] in
+        read_reg_neon_elem true r i ii >>= fun v ->
+        write_mem access_size addr v ii
+
       let build_semantics ii =
         M.addT (A.next_po_index ii.A.program_order_index)
           AArch64Base.(
@@ -880,6 +890,15 @@ module Make
             simd_op ADD MachSize.Quad r1 r2 r3 ii
 
         (* Neon loads and stores *)
+        | I_LD1(r1,i,rA,kr) ->
+            read_reg_ord rA ii >>= fun addr ->
+            (load_elem MachSize.S128 r1 i addr ii >>|
+            post_kr rA addr kr ii) >>! B.Next
+        | I_ST1(r1,i,rA,kr) ->
+            read_reg_ord rA ii >>= fun addr ->
+            (store_elem r1 i addr ii >>|
+            post_kr rA addr kr ii) >>! B.Next
+
         | I_LDR_SIMD(var,r1,rA,kr,s) ->
             let access_size = tr_simd_variant var in 
             get_ea rA kr s ii >>= fun addr ->
